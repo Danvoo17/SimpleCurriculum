ea2ef0897251ac41c01c79dbdfc874807ad8a742
Danvoo17 <warper.master25@gmail.com>
2025-03-29 11:29:41 -0400

    Renamed to SimpleCurriculum again

---
+++ SimpleCurriculum/src/main/java/reports/Experience.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reports;
+
+/**
+ *
+ * @author Ronny Trinidad
+ */
+public class Experience {
+    private String company;
+    private String position;
+
+    public Experience(String company, String position) {
+        this.company = company;
+        this.position = position;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+}
