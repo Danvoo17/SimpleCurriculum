a7cc4fe8fc57366f25f97df3314798072f103ec9
Danvoo17 <warper.master25@gmail.com>
2025-03-29 11:08:31 -0400

    Renamed main package, main class, project name and project folder to SimpleCurriculum (it was SimpleCurriculun, with N)

---
+++ SimpleCurriculum/src/main/java/reports/Reference.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reports;
+
+/**
+ *
+ * @author Ronny Trinidad
+ */
+public class Reference {
+    private String refname;
+    private String refphone;
+
+    public Reference(String refname, String refphone) {
+        this.refname = refname;
+        this.refphone = refphone;
+    }
+
+    public String getRefname() {
+        return refname;
+    }
+
+    public String getRefphone() {
+        return refphone;
+    }
+}
---
+++ SimpleCurriculum/src/main/java/simplecurriculum/SimpleCurriculum.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package simplecurriculum;
+
+import views.MainView;
+
+/**
+ *
+ * @author Ronny Trinidad
+ */
+public class SimpleCurriculum {
+
+    public static void main(String[] args) {
+        MainView open = new MainView();
+        open.setVisible(true);
+    }
+}
